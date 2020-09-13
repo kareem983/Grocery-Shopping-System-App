79fc5216b84e243079a3c209be32c82fc4fa81f9
kareem saeed <karimsaid026@gmail.com>
2020-09-13 02:14:07 +0200

    App Icon Added & App name Changed

--- Grocery Shopping System/app/src/main/java/com/example/groceryshoppingsystem/MainActivity.java
+++ Grocery Shopping System/app/src/main/java/com/example/groceryshoppingsystem/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.groceryshoppingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //toolbar
+        mToolBar=(Toolbar) findViewById(R.id.main_TooBar);
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setTitle("بقالة");
 
- }
+    }
 }
