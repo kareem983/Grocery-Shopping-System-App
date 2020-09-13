025d0019705c7c0d07525acd234796f98055443e
Ziad abdelnaby <ziadabdelnaby46@gmail.com>
2020-09-13 21:20:07 +0200

    Splash Screen added

---
+++ Grocery Shopping System/app/src/main/java/com/example/groceryshoppingsystem/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.example.groceryshoppingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private TextView txt , appText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN , WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        appText = findViewById(R.id.appSplashName);
+        txt = findViewById(R.id.welcome);
+
+        YoYo.with(Techniques.FlipInX)
+                .duration(3000)
+                .repeat(0)
+                .playOn(appText);
+        YoYo.with(Techniques.ZoomIn)
+                .duration(3000)
+                .repeat(0)
+                .playOn(txt);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this , MainActivity.class));
+                finish();
+            }
+        } ,  3500);
+    }
+}
