68e1ea6b8f73e25d83251748d9eaec4c01f9f466
kareem saeed <karimsaid026@gmail.com>
2020-09-18 21:17:42 +0200

    Fix Some bugs

---
+++ Grocery Shopping System/app/src/main/java/com/example/groceryshoppingsystem/UserProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.groceryshoppingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
